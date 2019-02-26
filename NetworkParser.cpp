24c4984bfd0dc6c322e2a33a534f36e0715b5f75
Ward Gauderis <wgauderis@outlook.com>
2019-02-26 10:32:32 +0100

    parser

---
+++ NetworkParser.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ward on 2/26/19.
+//
+
+#include "NetworkParser.h"
---
+++ NetworkParser.h
@@ -0,0 +1,14 @@
+//
+// Created by ward on 2/26/19.
+//
+
+#ifndef SIMULATION_NETWORKPARSER_H
+#define SIMULATION_NETWORKPARSER_H
+
+
+class NetworkParser {
+
+};
+
+
+#endif //SIMULATION_NETWORKPARSER_H
