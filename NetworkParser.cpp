195e7949479213b5b9f17764dbfd7fd7a334aad4
Ward Gauderis <wgauderis@outlook.com>
2019-02-26 10:33:40 +0100

    parser2

--- NetworkParser.cpp
+++ NetworkParser.cpp
@@ -1,5 +1,10 @@
-//
-// Created by ward on 2/26/19.
-//
+//============================================================================
+// Name        : NetworkParser.cpp
+// Author      : Ward Gauderis
+// Date        : 2/26/19
+// Version     : 
+// Copyright   : Project Software Engineering - BA1 Informatica - Ward Gauderis - University of Antwerp
+// Description : 
+//============================================================================
 
 #include "NetworkParser.h"
--- NetworkParser.h
+++ NetworkParser.h
@@ -1,6 +1,11 @@
-//
-// Created by ward on 2/26/19.
-//
+//============================================================================
+// Name        : NetworkParser.h
+// Author      : Ward Gauderis
+// Date        : 2/26/19
+// Version     : 
+// Copyright   : Project Software Engineering - BA1 Informatica - Ward Gauderis - University of Antwerp
+// Description : 
+//============================================================================
 
 #ifndef SIMULATION_NETWORKPARSER_H
 #define SIMULATION_NETWORKPARSER_H
