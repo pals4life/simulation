57dd47f46d6c9012aa3f19b75c586f54bc622f16
Sharogue <mano.marichal@gmail.com>
2019-02-27 10:25:11 +0100

    creation of the Network class

---
+++ src/Network.cpp
@@ -0,0 +1,8 @@
+//============================================================================
+// @name        : Network.cpp
+// @author      : Mano Marichal
+// @date        : 27.02.19
+// @version     : 
+// @copyright   : Project Software Engineering - BA1 Informatica - Mano Marichal - University of Antwerp
+// @description : 
+//============================================================================#include "Network.h"
---
+++ src/Network.h
@@ -0,0 +1,18 @@
+//============================================================================
+// @name        : Network.h
+// @author      : Mano Marichal
+// @date        : 27.02.19
+// @version     : 
+// @copyright   : Project Software Engineering - BA1 Informatica - Mano Marichal - University of Antwerp
+// @description : 
+//============================================================================
+#ifndef SIMULATION_NETWORK_H
+#define SIMULATION_NETWORK_H
+
+
+class Network {
+
+};
+
+
+#endif //SIMULATION_NETWORK_H
