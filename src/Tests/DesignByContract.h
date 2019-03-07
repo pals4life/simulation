c4b704f70bb3efbfa2cc566d9465590eb785672c
Ward Gauderis <wgauderis@outlook.com>
2019-03-07 11:04:22 +0100

    gitignore

---
+++ src/Tests/DesignByContract.h
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : DesignByContract.h
+// Author      : Serge Demeyer
+// Version     :
+// Copyright   : Project Software Engineering - BA1 Informatica - Serge Demeyer - University of Antwerp
+// Description : Declarations for design by contract in C++
+//============================================================================
+
+#include <assert.h>
+
+#define REQUIRE(assertion, what) \
+	if (!(assertion)) __assert (what, __FILE__, __LINE__)
+
+#define ENSURE(assertion, what) \
+	if (!(assertion)) __assert (what, __FILE__, __LINE__)
