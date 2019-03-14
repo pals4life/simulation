2dc5604d9e014fead51caef046d9600598e2e302
Thomas   Dooms is super cool <43652322+funkymaniac@users.noreply.github.com>
2019-03-14 13:25:24 +0100

    PAL

--- src/Car.h
+++ src/Car.h
@@ -16,17 +16,51 @@
 class Car : public IVehicle
 {
 public:
+    /**
+    * REQUIRE(velocity >= 0, "Velocity must be greater than 0");
+    * REQUIRE(position >= 0, "Position must be greater than 0");
+    * REQUIRE(!license.empty(), "License plate must be valid");
+    *
+    * ENSURE(this->properlyInitialized(), "Vehicle constructor must end in properlyInitialized state");
+    */
     Car(const std::string& license, double pos, double velocity);
 
+    /**
+    * REQUIRE(speedLimit > 0, "Speedlimit must be greater than 0");
+    * REQUIRE(this->properlyInitialized(), "moved vehicle must be properly initialized");
+    *
+    * ENSURE((fAcceleration >= fgkMinAcceleration) && (fAcceleration <= fgkMaxAcceleration), "Acceleration is too high / low");
+    */
     virtual void move(const IVehicle* next, double speedLimit);
 
+    /**
+    * REQUIRE(this->properlyInitialized(), "Car was not initialized when calling getVehicleLength");
+    */
     virtual double getVehicleLength() const;
+
+    /**
+    * REQUIRE(this->properlyInitialized(), "Vehicle was not initialized when calling getType");
+    */
     virtual EVehicleType getType() const;
 
+    /**
+    * REQUIRE(this->properlyInitialized(), "Car was not initialized when calling getMaxVelocity");
+    */
     virtual double getMaxVelocity() const;
+
+    /**
+    * REQUIRE(this->properlyInitialized(), "Car was not initialized when calling getMinVelocity");
+    */
     virtual double getMinVelocity() const;
 
+    /**
+    * REQUIRE(this->properlyInitialized(), "Car was not initialized when calling getMaxAcceleration");
+    */
     virtual double getMaxAcceleration() const;
+
+    /**
+    * REQUIRE(this->properlyInitialized(), "Car was not initialized when calling getMinAcceleration");
+    */
     virtual double getMinAcceleration() const;
 
 protected:
