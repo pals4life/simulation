ccd2efb41e18e2e579874a2dc5f79a12eaf2d818
Ward Gauderis <wgauderis@outlook.com>
2019-06-15 15:08:58 +0200

    silent

--- build_all.sh
+++ build_all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
-make -C engine &
-cmake CMakeLists.txt
-make &
+make -C engine >/dev/null 2>&1 &
+cmake CMakeLists.txt >/dev/null 2>&1
+make >/dev/null 2>&1 &
 wait
