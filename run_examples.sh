3916c79181c40a868321097edd81f005dd3441cb
Ward Gauderis <wgauderis@outlook.com>
2019-06-17 18:56:03 +0200

    examples

--- run_examples.sh
+++ run_examples.sh
@@ -2,7 +2,13 @@
 cd "$(dirname "$0")"
 ./build_all.sh
 
+./simulation inputfiles/spec1.0.xml
 ./simulation inputfiles/spec2.0.xml
 ./simulation_debug
+
+for file in inputfiles/use_case_test_files ; do
+    ./simulation $file
+done
+
 
 echo done
